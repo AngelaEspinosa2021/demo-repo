575713108970675660674e3313f750592936ef43
AngelaEspinosa2021 <82293975+AngelaEspinosa2021@users.noreply.github.com>
2022-04-16 15:39:12 -0500

    Se agrega el atributo correo a la clase EmpleadoBE (#3)

--- Proyecto.BE/EmpleadoBE.cs
+++ Proyecto.BE/EmpleadoBE.cs
@@ -3,7 +3,8 @@
    public class EmpleadoBE
     {
         public string Nombre { get; set; }
-        public int? Edad { get; set; }
+        public int Edad { get; set; }
+        public string correo { get; set;}
         public string apellido {  get; set; }
         public string telefono { get; set; }
         public string direccion { get; set;}
